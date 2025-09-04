aade0b0acdd501c4b1252b5b6ab520c12f1d88cf
Ocskó Lilla <168168776+olilla92@users.noreply.github.com>
2025-09-04 15:17:08 +0200

    Add files via upload

---
+++ fuggvenyek.js
@@ -0,0 +1,14 @@
+const numbers = [2,13,3,7,17,5,11,19,9]
+const names = ['Eva', 'Adel', 'Cedric', 'Dior', 'Frank', 'Bob']
+const fruits = ['pineapple', 'kiwi', 'banana', 'pear', 'cherry']
+
+//sortByLength() - String tömböt a szavak hossza szerint rendezi és a rendezett tömböz adja vissza
+
+
+// sortByLengthAsc() - String tömböt a szavak hossza szerint rendezi ABC sorrendbe és a rendezett tömböz adja vissza
+// sortFrom15() - számokat rendez a 15-től való távolság alapján és a rendezett tömböt adja vissza
+// addAsterisk() - String tömb mindegy elemének az elejére és végére egy csillagot tesz és visszaadja a módosított tömböt
+// between5And15() - számokat tartalmazó tömb 5 és 15 közötti elemeit adja vissza egy tömbben
+// isAllOdd() - számokat tartalmazó tömb minden eleme páratlan-e. Visszatérési érték true vagy false
+// hasEven() - számokat tartalmazó tömb tartalmaz-e páros elemet. Visszatérési érték true vagy false
+// sigma() - számokat tartalmazó tömb elemeit összeszorozza és a szorzatot adja vissza
