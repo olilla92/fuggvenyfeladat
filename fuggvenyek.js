0c42e12b88c4301ccdd8ec731ca641359d674f31
Ocskó Lilla <168168776+olilla92@users.noreply.github.com>
2025-09-09 19:19:52 +0200

    Update fuggvenyek.js
    Majdnem teljesen kész a feladat.

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -4,52 +4,48 @@ const fruits = ["pineapple", "kiwi", "banana", "pear", "cherry"];
 
 //sortByLength() - String tömböt a szavak hossza szerint rendezi és a rendezett tömböz adja vissza
 function sortByLength() {
-  for (let i = 0; i < numbers.length; i++) {}
+  return names.sort((a, b) => a.length - b.length) 
 }
-
+console.log("Nevek rendezve hossz szerint: " + sortByLength())
+function sortByLengthFruits(){
+    return fruits.sort((a, b) => a.length - b.length)
+}
+console.log("Gyümölcsök rendezve hossz szerint: " + sortByLengthFruits())
 // sortByLengthAsc() - String tömböt a szavak hossza szerint rendezi ABC sorrendbe és a rendezett tömböz adja vissza
+function sortByLengthAsc(){
 
+}
 // sortFrom15() - számokat rendez a 15-től való távolság alapján és a rendezett tömböt adja vissza
-
+function sortFrom15(){
+    let tav = [];
+    numbers.forEach((item) => {
+        tav.push(15 - Math.abs(item));
+    })
+    return tav.sort((a, b) => a - b)
+    
+}
+console.log("15-től való távolság: " + sortFrom15())
 // addAsterisk() - String tömb mindegy elemének az elejére és végére egy csillagot tesz és visszaadja a módosított tömböt
 function addAsterisk() {
-  const asterisk = [];
+  return names.map((item) => ' *'+item+'*')
 }
-console.log(addAsterisk());
+console.log("Nevek csillaggal: " + addAsterisk());
 
 // between5And15() - számokat tartalmazó tömb 5 és 15 közötti elemeit adja vissza egy tömbben
-function between5And15(ot) {
-  const otos = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] >= 5 && numbers[i] <= 15) {
-      otos.push(numbers[i]);
-    }
-  }
-  return otos;
+function between5And15() {
+  return numbers.filter((item) => item >= 5 && item <= 15)
 }
 console.log("5 és 15 között: " + between5And15());
 
 // isAllOdd() - számokat tartalmazó tömb minden eleme páratlan-e. Visszatérési érték true vagy false
 function isAllOdd() {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  return numbers.every((item) => item % 2 != 0)
 }
 console.log("Mind páratlan-e? " + isAllOdd());
 
 // hasEven() - számokat tartalmazó tömb tartalmaz-e páros elemet. Visszatérési érték true vagy false
 function hasEven() {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  return numbers.some((item) => item % 2 == 0)
 }
 console.log("Van-e közte páros szám? " + hasEven());
 
